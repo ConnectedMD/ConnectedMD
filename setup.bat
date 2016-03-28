4120f7f19d561a234ab0c61be8b00b980d2973ca
Mark Hebert <mark@hasissues.com>
2016-03-27 20:40:10 -0400

    add setup

---
+++ setup.bat
@@ -0,0 +1,10 @@
+call npm install -g gulp-cli
+call npm install -g grunt-cli
+call npm install -g cordova@6.0.0
+call npm install -g ionic@beta
+
+call npm install
+cd cordova
+call npm install
+call gulp build
+cd..
