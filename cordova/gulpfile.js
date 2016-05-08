13cd57bcce7769ae6203c759ca5cb147ec5c17df
Mark Hebert <mark@hasissues.com>
2016-05-08 14:31:45 -0400

    fix issues with ionic beta 25

--- cordova/gulpfile.js
+++ cordova/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     gulpWatch = require('gulp-watch'),
     del = require('del'),
+    runSequence = require('run-sequence'),
     argv = process.argv;
 
 
@@ -12,6 +13,7 @@ var gulp = require('gulp'),
 gulp.task('serve:before', ['watch']);
 gulp.task('emulate:before', ['build']);
 gulp.task('deploy:before', ['build']);
+gulp.task('build:before', ['build']);
 
 // we want to 'watch' when livereloading
 var shouldWatch = argv.indexOf('-l') > -1 || argv.indexOf('--livereload') > -1;
@@ -25,27 +27,46 @@ gulp.task('run:before', [shouldWatch ? 'watch' : 'build']);
  * changes, but you are of course welcome (and encouraged) to customize your
  * build however you see fit.
  */
-//var buildBrowserify = require('ionic-gulp-browserify-es2015');
-var buildWebpack = require('ionic-gulp-webpack-build');
+var buildBrowserify = require('ionic-gulp-browserify-es2015');
 var buildSass = require('ionic-gulp-sass-build');
 var copyHTML = require('ionic-gulp-html-copy');
 var copyFonts = require('ionic-gulp-fonts-copy');
 var copyScripts = require('ionic-gulp-scripts-copy');
 
-//gulp.task('watch', ['sass', 'html', 'fonts', 'scripts'], function(){
-gulp.task('watch', ['sass', 'html', 'fonts'], function(){
-  gulpWatch('app/**/*.scss', function(){ gulp.start('sass'); });
-  gulpWatch('app/**/*.html', function(){ gulp.start('html'); });
-  //return buildBrowserify({ watch: true });
-  return buildWebpack({ watch: true });
+var isRelease = argv.indexOf('--release') > -1;
+
+gulp.task('watch', ['clean'], function(done){
+  runSequence(
+    ['sass', 'html', 'fonts', 'scripts'],
+    function(){
+      gulpWatch('app/**/*.scss', function(){ gulp.start('sass'); });
+      gulpWatch('app/**/*.html', function(){ gulp.start('html'); });
+      buildBrowserify({ watch: true }).on('end', done);
+    }
+  );
 });
 
-//gulp.task('build', ['sass', 'html', 'fonts'], buildBrowserify);
-gulp.task('build', ['sass', 'html', 'fonts'], buildWebpack);
+gulp.task('build', ['clean'], function(done){
+  runSequence(
+    ['sass', 'html', 'fonts', 'scripts'],
+    function(){
+      buildBrowserify({
+        minify: isRelease,
+        browserifyOptions: {
+          debug: !isRelease
+        },
+        uglifyOptions: {
+          mangle: false
+        }
+      }).on('end', done);
+    }
+  );
+});
+
 gulp.task('sass', buildSass);
 gulp.task('html', copyHTML);
 gulp.task('fonts', copyFonts);
 gulp.task('scripts', copyScripts);
-gulp.task('clean', function(done){
-  del('www/build', done);
+gulp.task('clean', function(){
+  return del('www/build');
 });
